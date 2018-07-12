b94d5390ee22de5118beed697264015e1f9f0e9d
fuyao121701 <m13739732328@163.com>
2018-07-12 11:03:12 +0800

    "shopping"

--- KETANG/src/routes/shopping/Shopping.js
+++ KETANG/src/routes/shopping/Shopping.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux'
 import NavTop from '../../component/NavTop'
-import {Checkbox, Icon,InputNumber,BackTop,Radio} from 'antd'
+import {Checkbox, Icon,InputNumber,BackTop} from 'antd'
 import action from '../../store/action'
 import List from '../../component/List'
-import {removeShopCart} from "../../api/course";
+import {removeShopCart} from '../../api/course'
 import {Link} from 'react-router-dom'
 
 let img=require('../../static/images/dangdang.png');
@@ -13,14 +13,9 @@ class Shopping extends Component {
         super(props, context);
         this.state = {
             title: '购物车',
-            compile:false,
             money:0.00,
         }
     }
-    handleBit=()=>{
-        if (this.state.compile) return;
-        this.setState({compile:true})
-    };
     render(){
         return (
             <div>
@@ -45,7 +40,7 @@ class Shopping extends Component {
                                 {this.props.shopCart.unpay.map((item,index)=>{
                                     let {pic,name,price,pricing,id,pcs,check}=item;
                                     return <li className='shop_li' key={index}>
-                                        <Checkbox onChange={this.props.handleSelect.bind(this,id)} checked={check}></Checkbox>
+                                        <Checkbox onChange={this.props.handleSelect.bind(this,id)} checked={check} onClick={this.sum}></Checkbox>
                                         <Link to={{
                                             pathname: `/shopping/details`,
                                             search: `?courseId=${id}`
@@ -54,45 +49,24 @@ class Shopping extends Component {
                                                  style={{display: 'inline-block',width:'2rem',height:'2rem'}}
                                                  className='shop_img'/>
                                         </Link>
-                                        {
-                                            !this.state.compile?(<div className='shop_price'>
-                                                    <Link to={{
-                                                        pathname: `/shopping/details`,
-                                                        search: `?courseId=${id}`
-                                                    }}>
-                                                        <p>{name}</p>
-                                                    </Link>
-                                                    <span style={{color: '#ff2832'}}>{price}</span>
-                                                    <span
-                                                        style={{textDecoration: 'line-through',color: '#b8b8b8'}}
-                                                        className='shop_pic'>{pricing}</span>
-                                                    <div style={{marginBottom:'.2rem'}}>
-                                                        <span>X{pcs}</span>
-                                                        <Icon type="edit"
-                                                              onClick={this.handleBit}
-                                                              style={{fontSize:'.4rem'}}/></div>
-                                                </div>)
-                                                :(<div style={{width:'100%'}}>
-                                                    <div style={{float:'left',marginTop:'.4rem'}}
-                                                         onClick={this.sum}
-                                                    >
-                                                        <InputNumber
-                                                            size="large" min={1}
-                                                            defaultValue={1}
-                                                            style={{width:'2.5rem'}}
-
-                                                        />
-                                                        <div><Icon
-                                                            type="delete"
-                                                            style={{fontSize:'.43rem',float:'right',marginTop:'.5rem'}}
-                                                            onClick={this.handleRemove}
-                                                        /></div>
-                                                    </div>
-                                                    <div
-                                                        style={{float:'right',backgroundColor:'#f0f0f0',padding:'1rem .35rem',fontSize:'.3rem'}} onClick={()=>{this.setState({compile:false})}}
-                                                    >完成</div>
-                                                </div>)
-                                        }
+                                        <div className='shop_price'>
+                                            <Link to={{
+                                                pathname: `/shopping/details`,
+                                                search: `?courseId=${id}`
+                                            }}>
+                                                <p>{name}</p>
+                                            </Link>
+                                            <span style={{color: '#ff2832'}}>{price}</span>
+                                            <span
+                                                style={{textDecoration: 'line-through',color: '#b8b8b8'}}
+                                                className='shop_pic'>{pricing}</span>
+                                            <div style={{marginBottom:'.2rem'}} onClick={this.sum}>
+                                                <InputNumber size="small" min={1} defaultValue={1} onChange={this.handleTotal}/>
+                                                <Icon
+                                                    type="delete"
+                                                    style={{fontSize:'.43rem',float:'right',marginRight:'.1rem'}}
+                                                    onClick={this.handleRemove}/></div>
+                                        </div>
                                     </li>
                                 })}
 
@@ -111,37 +85,33 @@ class Shopping extends Component {
     handleRemove=()=>{
         let selectIDList = [];
         this.props.shopCart.unpay.forEach((item)=>{
-            if(item.check){
-                selectIDList.push(item.id);
-            }
-        })
-        if(selectIDList.length==0){
-            alert("没有要删除的内容");
-            return
-        }
+            selectIDList.push(item.id);
+        });
         selectIDList = selectIDList.map(courseID=>{
             return removeShopCart(courseID);
-        })
+        });
         Promise.all(selectIDList).then(()=>{
             this.props.queryUnpay();
         })
-    }
+    };
+
     sum=(e)=>{
 
         var target = e.target,
-           tarName = target.tagName,
-           tarClass = target.className;
+            tarName = target.tagName,
+            tarClass = target.className;
         let a = 0;
-        if(tarClass == "ant-input-number-handler-up-inner"){
 
+        if(tarClass == "ant-input-number-handler-up-inner"){
             this.props.shopCart.unpay.forEach((item,index)=>{
                 if(item.check){
                     let  price =item.price.substr(1);
-                  let count = parseFloat(document.getElementsByClassName("ant-input-number-input")[index].value);
-                  a = price*count;
-                   this.setState({
-                       money:a,
-                   })
+                    let count = parseFloat(document.getElementsByClassName("ant-input-number-input")[index].value);
+                   a += parseFloat((price*count).toFixed(2));
+                    // a= parseFloat(a)
+                    this.setState({
+                        money:a,
+                    })
                 }
             })
 
@@ -151,16 +121,30 @@ class Shopping extends Component {
 
             this.props.shopCart.unpay.forEach((item,index)=>{
                 if(item.check){
-                    let  price =item.price.substr(1);
-                    let count = parseFloat(document.getElementsByClassName("ant-input-number-input")[index].value);
-                    a = (price*count).toFixed(2);
-                    this.setState({
-                        money:a,
-                    })
+                        let  price =item.price.substr(1);
+                        let count = parseFloat(document.getElementsByClassName("ant-input-number-input")[index].value);
+                        a += parseFloat((price*count).toFixed(2));
+                        this.setState({
+                            money:a,
+                        })
                 }
             })
 
 
+        } else if(tarClass == "ant-checkbox-input"){
+
+            this.props.shopCart.unpay.forEach((item,index)=>{
+
+                if(item.check){
+                    let  price =item.price.substr(1);
+                    let count = parseFloat(document.getElementsByClassName("ant-input-number-input")[index].value);
+                    a = parseFloat((price*count).toFixed(2));
+                    this.setState({
+                        money:a,
+                    })
+                }
+
+            })
         }
 
     }
